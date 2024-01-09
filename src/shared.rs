fbe3f900fbec2edec93c6c86f360132664ba42ff
Ask Sødal <asksodal@gmail.com>
2024-01-10 00:17:53 +0100

    added histogram
    Added histogram function, and updated CLI. Also added LAB color.

--- src/shared.rs
+++ src/shared.rs
@@ -1,6 +1,20 @@
-#[derive(Default)]
-pub struct Color {
-    pub r: u8,
-    pub g: u8,
-    pub b: u8,
+pub mod color {
+
+    #[derive(Default, Copy, Clone, Debug)]
+    pub struct Rgb(pub [u8; 3]);
+
+    impl From<Lab> for Rgb {
+        fn from(value: Lab) -> Self {
+            todo!()
+        }
+    }
+
+    #[derive(Default, Copy, Clone, Debug)]
+    pub struct Lab([u8; 3]);
+
+    impl From<Rgb> for Lab {
+        fn from(value: Rgb) -> Self {
+            todo!()
+        }
+    }
 }
